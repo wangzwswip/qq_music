ea7ccc048f439888c2633a1fec14b94f6a76ff8a
wangzwswip <wangzw410@gmial.com>
2020-06-20 17:34:07 +0800

    插件安装

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import 'font-awesome/css/font-awesome.min.css'
+import { Button } from 'element-ui'
 
 Vue.use(Router)
+Vue.use(Button)
 
 export default new Router({
   routes: [
